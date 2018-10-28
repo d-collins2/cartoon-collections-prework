234162080d367bbcc84510d8f990862ec54ab307
Damon Collins <damon.c.p.collins@gmail.com>
2018-10-28 20:53:47 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,6 +11,7 @@ end
 def long_planeteer_calls(array)# code an argument here
   # Your code here
   array.any? {|call| call.length == 4} 
+  false 
 end
 
 def find_the_cheese# code an argument here
