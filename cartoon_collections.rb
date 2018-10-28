4fdbe9431da0b79e738d110f779aa9f54351989e
Damon Collins <damon.c.p.collins@gmail.com>
2018-10-28 20:52:10 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,12 +1,14 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(array)# code an argument here
   # Your code here
+  array.each_with_index { |name, idx| puts "#{idx + 1}. #{name}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
+  array.map {|el| el.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
 end
 
